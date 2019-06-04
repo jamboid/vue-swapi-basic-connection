4cf767cf14204349cdcbe2ffba40f7aba1261ce3
Jamie Boyd <enkizero@gmail.com>
2019-06-04 23:36:27 +0100

    Added a couple of more rendering unit tests for the FormTextField component

--- src/components/FormTextField/FormTextField.spec.js
+++ src/components/FormTextField/FormTextField.spec.js
@@ -5,7 +5,8 @@ const testProps = {
   id: "name",
   label: "Your Name",
   value: "Jamie",
-  placeholder: "e.g. Text"
+  placeholder: "e.g. Text",
+  status: "error"
 }
 
 describe('FormTextField.vue', () => {
@@ -36,10 +37,28 @@ describe('FormTextField.vue', () => {
 })
 
 describe('FormTextField.vue', () => {
-  it('gets props value', () => {
+  it('sets props.value correctly', () => {
     const textField = shallowMount(FormTextField, {
       propsData: testProps 
     })
     expect(textField.props('value')).toBe(testProps.value)
+  })
+})
+
+describe('FormTextField.vue', () => {
+  it('renders props.id as for attribute on the label element', () => {
+    const textField = shallowMount(FormTextField, {
+      propsData: testProps
+    })
+    expect(textField.find('label').attributes().for).toMatch(testProps.id)
+  })
+})
+
+describe('FormTextField.vue', () => {
+  it('renders props.status as modifying class on root element', () => {
+    const textField = shallowMount(FormTextField, {
+      propsData: testProps
+    })
+    expect(textField.classes()).toContain('cp_TextField--' + testProps.status);
   })
 })
